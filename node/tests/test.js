3b874cf8872ec3b72484d54976767a4bc0e3e3c8
PinkFlufflyLlama <Cocacola5010mc@gmail.com>
2020-02-24 20:20:32 +0000

    Made a more thurourgh test

--- node/tests/test.js
+++ node/tests/test.js
@@ -1,9 +1,42 @@
 let SimpleAI = require("../simpleAI-node.js");
+
+let tryFunction = (functionToRun, expectedOutput) => {
+   console.log(`Attempting to run ${functionToRun.name}()`);
+   try {
+      let output = functionToRun();
+      if (expectedOutput != undefined) {
+         if (output == expectedOutput) {
+            console.log("Success!");
+         } else {
+            console.log("Fail");
+            console.error("Incorrect Output");
+         }
+      } else {
+         console.log("Success!");
+      }
+   } catch (error) {
+      console.log("Error")
+   }
+};
+
+
 try {
-   console.log("attempting to make instance of SimpleAI class");
+   console.log("Attempting to make instance of SimpleAI class");
    let nn = new SimpleAI();
    console.log("Success!");
 } catch (error) {
    console.log("Fail");
    console.error(error);
 }
+
+tryFunction(nn.setLayerSizes([2,3,2]));
+
+tryFunction(nn.build());
+
+tryFunction(nn.randomizeWeights());
+
+tryFunction(nn.randomizeWeights());
+
+tryFunction(nn.evolve());
+
+tryFunction(nn.predict());
